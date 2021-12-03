5181a78f7d343e81e5b3f81c2aee7ed666cdf11b
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-12-03 18:11:13 +0800

    skill visual

--- New Unity Project/Assets/Scripts/Logic/ISkillVisual.cs
+++ New Unity Project/Assets/Scripts/Logic/ISkillVisual.cs
@@ -1,4 +1,9 @@
-﻿namespace Logic
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.UI;
+
+namespace Logic
 {
     public interface ISkillVisual
     {
@@ -7,5 +12,39 @@
         /// </summary>
         ISkill Skill { get; }
         
+        /// <summary>
+        /// Interface reference to the skill canvas
+        /// </summary>
+        Canvas SkillCanvas { get; }
+        
+        /// <summary>
+        /// Interface reference to the frames' glow image
+        /// </summary>
+        Image FrameGlowImage { get; }
+        
+        /// <summary>
+        /// Interface reference to the frames' light 2D source 
+        /// </summary>
+        Light2D FrameLight2D { get; }
+        
+        /// <summary>
+        /// Interface reference to the frames' graphic
+        /// </summary>
+        Image FrameGraphic { get; }
+        
+        /// <summary>
+        /// Interface reference to the skill graphic
+        /// </summary>
+        Image SkillGraphic { get; }
+        
+        /// <summary>
+        /// Interface reference to the skill cooldown image
+        /// </summary>
+        Image CooldownIcon { get; }
+
+        /// <summary>
+        /// Interface reference to the skill cooldown text
+        /// </summary>
+        TextMeshProUGUI CooldownText { get; }
     }
 }
--- New Unity Project/Assets/Scripts/Logic/SkillVisual.cs
+++ New Unity Project/Assets/Scripts/Logic/SkillVisual.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using JetBrains.Annotations;
 using ScriptableObjectScripts;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.UI;
 using Object = UnityEngine.Object;
 
 namespace Logic
@@ -22,6 +25,81 @@ namespace Logic
             get => skill as ISkill;
             set => skill = value as Object;
         }
+        
+        /// <summary>
+        /// Reference to skill canvas
+        /// </summary>
+        [SerializeField]
+        private Canvas skillCanvas;
+        
+        public Canvas SkillCanvas
+        {
+            get => skillCanvas;
+            set => skillCanvas = value;
+        }
+        
+        /// <summary>
+        /// Reference to the frames' glow image component
+        /// </summary>
+        [SerializeField] private Image frameGlowImage;
+        public Image FrameGlowImage
+        {
+            get => frameGlowImage;
+            set => frameGlowImage = value;
+        }
+        
+        /// <summary>
+        /// Reference to the glow light 2D component
+        /// </summary>
+        [SerializeField] private Light2D frameLight2D;
+        public Light2D FrameLight2D
+        {
+            get => frameLight2D;
+            set => frameLight2D = value;
+        }
+        
+        /// <summary>
+        /// Reference to the frames' graphic image
+        /// </summary>
+        [SerializeField] private Image frameGraphic;
+        public Image FrameGraphic
+        {
+            get => frameGraphic;
+            set => frameGraphic = value;
+        }
+        
+        /// <summary>
+        /// Reference to the skill graphic image
+        /// </summary>
+        [SerializeField] private Image skillGraphic;
+        public Image SkillGraphic
+        {
+            get => skillGraphic;
+            set => skillGraphic = value;
+        }
+        
+        /// <summary>
+        /// Reference to the skill cooldown image
+        /// </summary>
+        [SerializeField] private Image cooldownIcon;
+        public Image CooldownIcon
+        {
+            get => cooldownIcon;
+            set => cooldownIcon = value;
+        }
+
+        /// <summary>
+        /// Reference to the skill cooldown text
+        /// </summary>
+        [SerializeField] private TextMeshProUGUI cooldownText;
+        public TextMeshProUGUI CooldownText
+        {
+            get => cooldownText;
+            set => cooldownText = value;
+        }
+        
+
+
 
     }
 }
