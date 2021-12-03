50475fa81f088cea69b73424eca616331ea2f4eb
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-12-04 06:05:33 +0800

    skill preview

--- New Unity Project/Assets/Scripts/Logic/ISkillPreview.cs
+++ New Unity Project/Assets/Scripts/Logic/ISkillPreview.cs
@@ -1,3 +1,7 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
 namespace Logic
 {
     public interface ISkillPreview
@@ -7,5 +11,37 @@ namespace Logic
         /// Interface reference to skill
         /// </summary>
         ISkill Skill { get; }
+        
+        /// <summary>
+        /// Interface reference to skill preview canvas
+        /// </summary>
+        Canvas PreviewCanvas { get; }
+
+        /// <summary>
+        /// Interface reference to skill preview frame Image
+        /// </summary>
+        Image FrameImage { get; set; }
+
+        /// <summary>
+        /// Interface reference to skill preview graphic image
+        /// </summary>
+        Image GraphicImage { get; set; }
+        
+        /// <summary>
+        /// Interface reference to skill preview cooldown
+        /// </summary>
+        TextMeshProUGUI Cooldown { get;  }
+        
+        /// <summary>
+        /// Interface reference to skill preview name
+        /// </summary>
+        TextMeshProUGUI PreviewName { get;  }
+        
+        /// <summary>
+        /// Interface reference to skill preview description
+        /// </summary>
+        TextMeshProUGUI Description { get; }
+
+
     }
 }
--- New Unity Project/Assets/Scripts/Logic/SkillPreview.cs
+++ New Unity Project/Assets/Scripts/Logic/SkillPreview.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using JetBrains.Annotations;
 using ScriptableObjectScripts;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Object = UnityEngine.Object;
 
 namespace Logic
@@ -16,11 +18,76 @@ namespace Logic
         /// </summary>
         [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(ISkill))]
         private Object skill;
-        
         public ISkill Skill
         {
             get => skill as ISkill;
             set => skill = value as Object;
         }
+        
+        /// <summary>
+        /// Reference to skill preview canvas
+        /// </summary>
+        [SerializeField] private Canvas previewCanvas;
+        public Canvas PreviewCanvas
+        {
+            get => previewCanvas;
+            set => previewCanvas = value;
+        }
+        
+        
+
+        /// <summary>
+        /// Reference to frame Image
+        /// </summary>
+        [SerializeField] private Image frameImage;
+        public Image FrameImage
+        {
+            get => frameImage;
+            set => frameImage = value;
+        }
+        
+        /// <summary>
+        /// Reference to the skill previews' graphic Image
+        /// </summary>
+        [SerializeField] private Image graphicImage;
+        public Image GraphicImage
+        {
+            get => graphicImage;
+            set => graphicImage = value;
+        }
+        
+        /// <summary>
+        /// Reference to skill preview cooldown
+        /// </summary>
+        [SerializeField] private TextMeshProUGUI cooldown;
+        public TextMeshProUGUI Cooldown
+        {
+            get => cooldown;
+            set => cooldown = value;
+        }
+        
+        /// <summary>
+        /// Reference to skill preview name
+        /// </summary>
+        [SerializeField] private TextMeshProUGUI previewName;
+        public TextMeshProUGUI PreviewName
+        {
+            get => previewName;
+            set => previewName = value;
+        }
+        
+        /// <summary>
+        /// Reference to skill preview name
+        /// </summary>
+        [SerializeField] private TextMeshProUGUI description;
+        public TextMeshProUGUI Description
+        {
+            get => description;
+            set => description = value;
+        }
+
+
+
+
     }
 }
