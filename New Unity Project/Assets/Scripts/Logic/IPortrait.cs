66b6cf993eab33a448871d29594598a4ccd8392e
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-12-06 05:39:40 +0800

    portrait prefab

---
+++ New Unity Project/Assets/Scripts/Logic/IPortrait.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Logic
+{
+    public interface IPortrait
+    {
+        /// <summary>
+        /// Interface reference to portrait canvas.
+        /// Set in the inspector.
+        /// </summary>
+        Canvas PortraitCanvas { get; }
+        
+        /// <summary>
+        /// Interface reference to portrait frame image.
+        /// Set in the inspector.
+        /// </summary>
+        Image FrameImage { get; }
+
+        /// <summary>
+        /// Interface reference to portrait image.
+        /// Set in the inspector.
+        /// </summary>
+        Image PortraitImage { get; set; }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Logic/Portrait.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Logic
+{
+    public class Portrait : MonoBehaviour, IPortrait
+    {
+        /// <summary>
+        /// Reference to portrait canvas.
+        /// Set in the inspector.
+        /// </summary>
+        [SerializeField] private Canvas portraitCanvas;
+        public Canvas PortraitCanvas
+        {
+            get => portraitCanvas;
+            set => portraitCanvas = value;
+        }
+        
+        /// <summary>
+        /// Reference to portrait frame image.
+        /// Set in the inspector.
+        /// </summary>
+        [SerializeField] private Image frameImage;
+        public Image FrameImage
+        {
+            get => frameImage;
+            set => frameImage = value;
+        }
+        
+        /// <summary>
+        /// Reference to portrait image.
+        /// Set in the inspector.
+        /// </summary>
+        [SerializeField] private Image portraitImage;
+        public Image PortraitImage
+        {
+            get => portraitImage;
+            set => portraitImage = value;
+        }
+
+    }
+}
