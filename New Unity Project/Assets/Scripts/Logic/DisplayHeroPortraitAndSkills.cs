b56d99333333942cd2e65984e4dae4de1a3ee652
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-12-14 16:43:30 +0800

    Update DisplayHeroPortraitAndSkills.cs

--- New Unity Project/Assets/Scripts/Logic/DisplayHeroPortraitAndSkills.cs
+++ New Unity Project/Assets/Scripts/Logic/DisplayHeroPortraitAndSkills.cs
@@ -18,19 +18,39 @@ namespace Logic
 
         private void OnMouseDown()
         {
-            var displayedPortraitAndSkills = _targetCollider.Hero.Player.AliveHeroes.DisplayedPortraitAndSkills;
-            
-            if (displayedPortraitAndSkills.DisplayedHeroPortrait !=null)
-                displayedPortraitAndSkills.DisplayedHeroPortrait.ThisGameObject.SetActive(false);
-            if (displayedPortraitAndSkills.DisplayedHeroSkills != null)
-                displayedPortraitAndSkills.DisplayedHeroSkills.ThisGameObject.SetActive(false);
+            DisplayPortraitAndSkillsThisPlayer();
+            DisplayPortraitAndSkillsOtherPlayer();
+        }
+
+        private void DisplayPortraitAndSkillsThisPlayer()
+        {
+            var displayPortraitAndSkills = _targetCollider.Hero.Player.AliveHeroes.DisplayedPortraitAndSkills;
+
+            displayPortraitAndSkills.DisplayedHeroPortrait?.ThisGameObject.SetActive(false);
+            displayPortraitAndSkills.DisplayedHeroSkills?.ThisGameObject.SetActive(false);
             
             _targetCollider.Hero.DisplayHeroPortrait.ThisGameObject.SetActive(true);
             _targetCollider.Hero.DisplayHeroPortrait.TogglePortraitDisplay.ShowPortrait();
             _targetCollider.Hero.DisplayHeroSkills.ThisGameObject.SetActive(true);
 
-            displayedPortraitAndSkills.DisplayedHeroPortrait = _targetCollider.Hero.DisplayHeroPortrait;
-            displayedPortraitAndSkills.DisplayedHeroSkills = _targetCollider.Hero.DisplayHeroSkills;
+            displayPortraitAndSkills.DisplayedHeroPortrait = _targetCollider.Hero.DisplayHeroPortrait;
+            displayPortraitAndSkills.DisplayedHeroSkills = _targetCollider.Hero.DisplayHeroSkills;
+        }
+        
+        private void DisplayPortraitAndSkillsOtherPlayer()
+        {
+            var displayPortraitAndSkills = _targetCollider.Hero.Player.OtherPlayer.AliveHeroes.DisplayedPortraitAndSkills;
+
+            displayPortraitAndSkills.DisplayedHeroPortrait?.ThisGameObject.SetActive(false);
+            displayPortraitAndSkills.DisplayedHeroSkills?.ThisGameObject.SetActive(false);
+            
+            _targetCollider.Hero.DisplayHeroPortrait.ThisGameObject.SetActive(true);
+            _targetCollider.Hero.DisplayHeroPortrait.TogglePortraitDisplay.ShowPortrait();
+            _targetCollider.Hero.DisplayHeroSkills.ThisGameObject.SetActive(true);
+
+            displayPortraitAndSkills.DisplayedHeroPortrait = _targetCollider.Hero.DisplayHeroPortrait;
+            displayPortraitAndSkills.DisplayedHeroSkills = _targetCollider.Hero.DisplayHeroSkills;
+
         }
     }
 }
