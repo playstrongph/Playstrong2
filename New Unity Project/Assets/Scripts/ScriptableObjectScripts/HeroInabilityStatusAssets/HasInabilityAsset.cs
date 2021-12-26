a8df4736e73297fd16889d0bd404ac3904338666
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-12-26 17:13:41 +0800

    Update HasInabilityAsset.cs

--- New Unity Project/Assets/Scripts/ScriptableObjectScripts/HeroInabilityStatusAssets/HasInabilityAsset.cs
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/HeroInabilityStatusAssets/HasInabilityAsset.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 namespace ScriptableObjectScripts.HeroInabilityStatusAssets
 {
+    
     [CreateAssetMenu(fileName = "HasInability", menuName = "Assets/HeroInabilityStatus/HasInability")]
     public class HasInabilityAsset : HeroInabilityStatusAsset
-    {   
+    {
+        /// <summary>
+        /// Hero misses his turn and proceed to after hero end turn
+        /// </summary>
+        /// <param name="turnController"></param>
+        /// <returns></returns>
         public override IEnumerator StatusAction(ITurnController turnController)
         {
             var logicTree = turnController.CoroutineTrees.MainLogicTree;
