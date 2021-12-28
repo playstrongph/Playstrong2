64ed320b3c7e265d36aa185c71a6e93c6ba0144c
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-12-28 14:55:58 +0800

    Skill Type Asset Base Class

--- New Unity Project/Assets/Scripts/ScriptableObjectScripts/SkillTypeAssets/ISkillTypeAsset.cs
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/SkillTypeAssets/ISkillTypeAsset.cs
@@ -1,58 +1,40 @@
 ﻿using Logic;
 using TMPro;
-using UnityEngine;
 
 namespace ScriptableObjectScripts.SkillTypeAssets
 {
-    /// <summary>
-    /// Provides logic for logic execution of skill cooldown, skill readiness, and skill enabled status
-    /// </summary>
-    public abstract class SkillTypeAsset : ScriptableObject, ISkillTypeAsset
+    public interface ISkillTypeAsset
     {
-        #region SkillCooldown
-        
         /// <summary>
         /// Active skills enabled, disabled if cooldown is less or equal to zero/
         /// Basic Skills disabled/ Passive Skill enabled, disabled if cooldown
         /// less or equal to zero
         /// </summary>
         /// <param name="text"></param>
-        public virtual void SkillCooldownTextDisplay(TextMeshProUGUI text)
-        {
-        }
-        
+        void SkillCooldownTextDisplay(TextMeshProUGUI text);
+
         /// <summary>
         /// Reduces skill cooldown based on the skill cooldown type - 
         /// Normal cooldown, immutable cooldown, and no skill cooldown
         /// </summary>
         /// <param name="skill"></param>
         /// <param name="counter"></param>
-        public virtual void ReduceSkillCooldown(ISkill skill, int counter)
-        {
-        }   
-        
+        void ReduceSkillCooldown(ISkill skill, int counter);
+
         /// <summary>
         /// Resets skill to max skill cooldown based on skill cooldown type - 
         /// Normal cooldown, immutable cooldown, and no skill cooldown
         /// </summary>
         /// <param name="skill"></param>
-        public virtual void ResetSkillCooldown(ISkill skill)
-        {
-        }
-        
+        void ResetSkillCooldown(ISkill skill);
+
         /// <summary>
         /// Sets skill cooldown to a specific value (clamped between zero and max skill CD)
         /// based on skill cooldown type - Normal, immutable, or no skill cooldown
         /// </summary>
         /// <param name="skill"></param>
         /// <param name="counter"></param>
-        public virtual void SetSkillCooldownValue(ISkill skill, int counter)
-        {
-        }
-        
-        #endregion  
-
-        #region SkillReadiness
+        void SetSkillCooldownValue(ISkill skill, int counter);
 
         /// <summary>
         /// Skill Readiness checks skill enabled status and executes status action depending on skill type
@@ -60,10 +42,8 @@ namespace ScriptableObjectScripts.SkillTypeAssets
         /// Passive Skills - hides cooldown text  
         /// </summary>
         /// <param name="skill"></param>
-        public virtual void SetSkillReady(ISkill skill)
-        {
-        }
-        
+        void SetSkillReady(ISkill skill);
+
         /// <summary>
         /// Skill Readiness status action depending on skill type
         /// Active and Basic Skills - Disables select drag target, enables target visual,
@@ -71,32 +51,20 @@ namespace ScriptableObjectScripts.SkillTypeAssets
         /// Passive Skills - shows cooldown text if CD > 0 
         /// </summary>
         /// <param name="skill"></param>
-        public virtual void SetSkillNotReady(ISkill skill)
-        {
-        }
-        
-        #endregion
+        void SetSkillNotReady(ISkill skill);
 
-        #region SkillEnabledDisabled
-        
         /// <summary>
         /// Used by 'Silence' and 'Seal' type effects - sets skill enabled status to skill disabled and
         /// executes skill disabled status actions
         /// </summary>
         /// <param name="skill"></param>
-        public virtual void DisableSkill(ISkill skill)
-        {
-        }
-        
+        void DisableSkill(ISkill skill);
+
         /// <summary>
         /// Used by 'Silence' and 'Seal' type effects - sets skill enabled status to skill enabled and
         /// executes skill enabled status actions
         /// </summary>
         /// <param name="skill"></param>
-        public virtual void EnableSkill(ISkill skill)
-        {
-        }
-        
-        #endregion
+        void EnableSkill(ISkill skill);
     }
 }
