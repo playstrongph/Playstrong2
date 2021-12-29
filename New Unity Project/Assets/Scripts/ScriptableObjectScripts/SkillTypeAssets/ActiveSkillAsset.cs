7323781137bfa18125a0ba2de47ec3208ba97167
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-12-29 17:14:27 +0800

    Update ActiveSkillAsset.cs

--- New Unity Project/Assets/Scripts/ScriptableObjectScripts/SkillTypeAssets/ActiveSkillAsset.cs
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/SkillTypeAssets/ActiveSkillAsset.cs
@@ -7,10 +7,42 @@ namespace ScriptableObjectScripts.SkillTypeAssets
     [CreateAssetMenu(fileName = "ActiveSkill", menuName = "Assets/SkillType/ActiveSkill")]
     public class ActiveSkillAsset : SkillTypeAsset
     {
+        
+        /// <summary>
+        /// Displays skill glow and enables hero skill targeting
+        /// </summary>
+        /// <param name="skill"></param>
         public override void SkillReadyActions(ISkill skill)
         {
+            //Enable get skill target actions - show valid targets glow
+            skill.SkillTargetCollider.GetSkillTargets.EnableGetSkillTargetActions();
             
+            //Enable select drag target actions - set hero targets and use hero skill
+            skill.SkillTargetCollider.SelectDragTarget.EnableSelectDragTargetActions();
+            
+            //Show skill glows
+            skill.SkillVisual.SkillGlowDisplay.ShowGlow();
         }
+        
+        /// <summary>
+        /// Hides skill glow and disables hero skill targeting
+        /// </summary>
+        /// <param name="skill"></param>
+        public override void SkillNotReadyActions(ISkill skill)
+        {
+            //Enable get skill target actions - show valid targets glow
+            skill.SkillTargetCollider.GetSkillTargets.DisableGetSkillTargetsActions();
+            
+            //Enable select drag target actions - set hero targets and use hero skill
+            skill.SkillTargetCollider.SelectDragTarget.DisableSelectDragTargetActions();
+            
+            //Show skill glows
+            skill.SkillVisual.SkillGlowDisplay.HideGlow();
+        }
+        
+        
+        
+        
 
 
     }
