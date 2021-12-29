0a9261acde178303ac8f8273c9c56d1c2881366e
playstrongph <44939646+playstrongph@users.noreply.github.com>
2021-12-29 17:53:42 +0800

    Skill Types

--- New Unity Project/Assets/Scripts/ScriptableObjectScripts/SkillTypeAssets/HybridSkillAsset.cs
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/SkillTypeAssets/HybridSkillAsset.cs
@@ -3,7 +3,10 @@ using TMPro;
 using UnityEngine;
 
 namespace ScriptableObjectScripts.SkillTypeAssets
-{
+{   
+    /// <summary>
+    /// Passive skills with cooldown
+    /// </summary>
     [CreateAssetMenu(fileName = "HybridSkill", menuName = "Assets/SkillType/HybridSkill")]
     public class HybridSkillAsset : SkillTypeAsset
     {
