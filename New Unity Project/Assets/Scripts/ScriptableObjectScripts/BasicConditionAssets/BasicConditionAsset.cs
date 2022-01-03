43b90b80fdbc650812ea4bcf176a77d066587a0f
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-01-03 15:03:51 +0800

    Basic Condition Asset

---
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/BasicConditionAssets/BasicConditionAsset.cs
@@ -0,0 +1,52 @@
+﻿using Logic;
+using UnityEngine;
+
+namespace ScriptableObjectScripts.BasicConditionAssets
+{
+    public abstract class BasicConditionAsset : ScriptableObject, IBasicConditionAsset
+    {      
+        /// <summary>
+        /// The skill originator of the basic condition
+        /// </summary>
+        protected ISkill SkillParent;
+
+        /// <summary>
+        /// Overriden by the deriving classes that implements their logic
+        /// E.g. ConditionUsingSpecificSkill
+        /// </summary>
+        /// <param name="hero"></param>
+        /// <returns></returns>
+        protected virtual int CheckConditionValue(IHero hero)
+        {
+            return 0;
+        }
+        
+        
+        /// <summary>
+        /// If Check condition logic is met, returns 1
+        /// If Check condition logic is not met, returns 0
+        /// </summary>
+        /// <param name="hero"></param>
+        /// <returns></returns>
+        public int ConditionValue(IHero hero)
+        {
+            return CheckConditionValue(hero);
+        }
+
+        /// <summary>
+        /// Set the skill parent during initialization
+        /// 
+        /// </summary>
+        /// <param name="skill"></param>
+        public void InitializeSkillParent(ISkill skill)
+        {
+            SkillParent = skill;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/BasicConditionAssets/IBasicConditionAsset.cs
@@ -0,0 +1,22 @@
+﻿using Logic;
+
+namespace ScriptableObjectScripts.BasicConditionAssets
+{
+    public interface IBasicConditionAsset
+    {
+        /// <summary>
+        /// If Check condition logic is met, returns 1
+        /// If Check condition logic is not met, returns 0
+        /// </summary>
+        /// <param name="hero"></param>
+        /// <returns></returns>
+        int ConditionValue(IHero hero);
+
+        /// <summary>
+        /// Set the skill parent during initialization
+        /// 
+        /// </summary>
+        /// <param name="skill"></param>
+        void InitializeSkillParent(ISkill skill);
+    }
+}
