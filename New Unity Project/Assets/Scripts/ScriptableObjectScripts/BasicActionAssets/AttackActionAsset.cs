0cb2fda72d0d2766db7ab4ad4e805670b46c3e5a
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-01-07 18:35:10 +0800

    Attack Action WIP
    Pending: Deal Damage, Attack Animation, Attack Types

--- New Unity Project/Assets/Scripts/ScriptableObjectScripts/BasicActionAssets/AttackActionAsset.cs
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/BasicActionAssets/AttackActionAsset.cs
@@ -45,17 +45,54 @@ namespace ScriptableObjectScripts.BasicActionAssets
         
         private void AttackHero(IHero hero)
         {
-            //Before hero attacks events
-            PreSkillAttackEvents(hero);
-            PreAttackEvents(hero);
+            var logicTree = hero.CoroutineTrees.MainLogicTree;
             
-            //TODO: private set normal or critical attack
+            //Before hero attacks events
+            logicTree.AddCurrent(PreSkillAttackEvents(hero));
+            logicTree.AddCurrent(PreAttackEvents(hero));
+
+            logicTree.AddCurrent(NormalOrCriticalAttack(hero));
             
             //After hero attacks events
-            PostAttackEvents(hero);
-            PostSkillAttackEvents(hero);
-            
+            logicTree.AddCurrent(PostAttackEvents(hero));
+            logicTree.AddCurrent(PostSkillAttackEvents(hero));
         }
+        
+        /// <summary>
+        /// Determines if attack is a critical strike based on critical chance and resistance
+        /// Has to be a coroutine due to events
+        /// </summary>
+        /// <param name="casterHero"></param>
+        private IEnumerator NormalOrCriticalAttack(IHero casterHero)
+        {
+            var logicTree = casterHero.CoroutineTrees.MainLogicTree;
+            var targetedHero = casterHero.HeroLogic.LastHeroTargets.TargetedHero;
+            var criticalChance = casterHero.HeroLogic.ChanceAttributes.CriticalChance + skillCriticalChance;
+            var criticalResistance = targetedHero.HeroLogic.ResistanceAttributes.CriticalResistance;
+            var netChance = criticalChance - criticalResistance;
+            var randomChance = Random.Range(1, 101);
+            
+            
+            if(randomChance<=netChance)
+                CriticalAttack(casterHero);
+            else
+                NormalAttack(casterHero);
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+        private void NormalAttack(IHero casterHero)
+        {
+            //TODO: DealDamage 
+        }
+        
+        private void CriticalAttack(IHero casterHero)
+        {
+            //TODO: DealDamage
+        }
+        
+        
 
         #region EVENTS
         
@@ -63,37 +100,67 @@ namespace ScriptableObjectScripts.BasicActionAssets
         /// Before hero skill attacks event
         /// </summary>
         /// <param name="casterHero"></param>
-        private void PreSkillAttackEvents(IHero casterHero)
+        private IEnumerator PreSkillAttackEvents(IHero casterHero)
         {
+            var logicTree = casterHero.CoroutineTrees.MainLogicTree;
             var targetedHero = casterHero.HeroLogic.LastHeroTargets.TargetedHero;
             
             casterHero.HeroLogic.HeroEvents.EventBeforeHeroSkillAttacks(casterHero);
             targetedHero.HeroLogic.HeroEvents.EventEBeforeHeroIsSkillAttacked(targetedHero);
+            
+            logicTree.EndSequence();
+            yield return null;
         }
         
-        private void PreAttackEvents(IHero casterHero)
+        /// <summary>
+        /// Before hero attacks event
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <returns></returns>
+        private IEnumerator PreAttackEvents(IHero casterHero)
         {
+            var logicTree = casterHero.CoroutineTrees.MainLogicTree;
             var targetedHero = casterHero.HeroLogic.LastHeroTargets.TargetedHero;
             
             casterHero.HeroLogic.HeroEvents.EventBeforeHeroAttacks(casterHero);
             targetedHero.HeroLogic.HeroEvents.EventBeforeHeroIsAttacked(casterHero);
+            
+            logicTree.EndSequence();
+            yield return null;
         }
         
-        private void PostAttackEvents(IHero casterHero)
+        /// <summary>
+        /// After hero attacks event
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <returns></returns>
+        private IEnumerator PostAttackEvents(IHero casterHero)
         {
+            var logicTree = casterHero.CoroutineTrees.MainLogicTree;
             var targetedHero = casterHero.HeroLogic.LastHeroTargets.TargetedHero;
             
             casterHero.HeroLogic.HeroEvents.EventAfterHeroAttacks(casterHero);
             targetedHero.HeroLogic.HeroEvents.EventAfterHeroIsAttacked(targetedHero);
+            
+            logicTree.EndSequence();
+            yield return null;
         }
         
-        private void PostSkillAttackEvents(IHero casterHero)
+        /// <summary>
+        /// After hero skill attacks event
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <returns></returns>
+        private IEnumerator PostSkillAttackEvents(IHero casterHero)
         {
+            var logicTree = casterHero.CoroutineTrees.MainLogicTree;
             var targetedHero = casterHero.HeroLogic.LastHeroTargets.TargetedHero;
             
             casterHero.HeroLogic.HeroEvents.EventAfterHeroSkillAttacks(casterHero);
             targetedHero.HeroLogic.HeroEvents.EventAfterHeroIsSkillAttacked(targetedHero);
-
+            
+            logicTree.EndSequence();
+            yield return null;
         }
         
         
