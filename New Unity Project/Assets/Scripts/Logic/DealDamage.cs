466f8726d8c76da9fc8636bd2c3e60a17b873331
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-01-07 19:24:24 +0800

    Update DealDamage.cs

--- New Unity Project/Assets/Scripts/Logic/DealDamage.cs
+++ New Unity Project/Assets/Scripts/Logic/DealDamage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Logic
@@ -12,6 +13,48 @@ namespace Logic
             _heroLogic = GetComponent<IHeroLogic>();
         }
         
+        public IEnumerator DealSingleAttackDamage(IHero casterHero, int nonCriticalDamage, int criticalDamage)
+        {
+            var logicTree = casterHero.CoroutineTrees.MainLogicTree;
+            var targetedHero = casterHero.HeroLogic.LastHeroTargets.TargetedHero;
+            
+            
+            //TODO: var finalNonCriticalDamage = ComputeSingleAttackNonCriticalDamage(nonCriticalDamage);
+            
+            //TODO: var finalCriticalDamage = ComputeSingleAttackCriticalDamage(criticalDamage);
+            
+            logicTree.AddCurrent(EventBeforeHeroDealsSkillDamage(casterHero));
+            
+            //TODO: logicTree.AddCurrent(targetHero.HeroLogic.TakeDamageTest.TakeSingleAttackDamage(finalNonCriticalDamage, finalCriticalDamage,attackerHero));
+            
+            logicTree.AddCurrent(EventAfterHeroDealsSkillDamage(casterHero));
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+        #region EVENTS
+
+        private IEnumerator EventBeforeHeroDealsSkillDamage(IHero casterHero)
+        {
+            var logicTree = casterHero.CoroutineTrees.MainLogicTree;
+            
+            casterHero.HeroLogic.HeroEvents.EventBeforeHeroDealsSkillDamage(casterHero);
+
+            logicTree.EndSequence();
+            yield return null;
+        }
         
+        private IEnumerator EventAfterHeroDealsSkillDamage(IHero casterHero)
+        {
+            var logicTree = casterHero.CoroutineTrees.MainLogicTree;
+            
+            casterHero.HeroLogic.HeroEvents.EventAfterHeroDealsSkillDamage(casterHero);
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+        #endregion
     }
 }
