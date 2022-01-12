9f0d2813a47b5be34e67040f2e9e185b5c3e529d
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-01-12 12:09:43 +0800

    Damage Animation WIP

---
+++ New Unity Project/Assets/Scripts/Logic/DamageAnimation.cs
@@ -0,0 +1,67 @@
+﻿using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Logic
+{
+    public class DamageAnimation : MonoBehaviour
+    {
+        /// <summary>
+        /// Damage animation canvas
+        /// </summary>
+        [SerializeField] private Canvas canvas;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        [SerializeField] private CanvasGroup canvasGroup;
+        
+        /// <summary>
+        /// Damage text
+        /// </summary>
+        [SerializeField] private TextMeshProUGUI text;
+
+        /// <summary>
+        /// Damage cloud
+        /// </summary>
+        [SerializeField] private Image image;
+
+
+        #region TWEENER VALUES
+        
+        /// <summary>
+        /// Duration of the change scale animation 
+        /// </summary>
+        [SerializeField] private float doScaleDuration = 0.2f;
+        
+        
+        [SerializeField] private float localScaleMultiplier = 1.5f;
+        [SerializeField] private int doScaleLoopCount = 4;
+        [SerializeField] private float fadeInterval = 0.2f;
+        [SerializeField] private float fadeAlphaStart = 1.0f;
+        [SerializeField] private float fadeAlphaEnd = 0.0f;
+
+        #endregion
+
+        /// <summary>
+        /// Plays the damage animation
+        /// </summary>
+        /// <param name="damageText"></param>
+        public void PlayAnimation(int damageText)
+        {
+            //Display damage animation
+            canvasGroup.alpha = fadeAlphaStart;
+
+            //Display damage text
+            text.text = "-" + damageText.ToString();
+            
+            //Bounce image animation
+            transform.DOScale(transform.localScale * localScaleMultiplier, doScaleDuration)
+                .SetLoops(doScaleLoopCount, LoopType.Yoyo).SetEase(Ease.InOutQuad);
+            
+            //Fade the damage image
+            canvasGroup.DOFade(fadeAlphaEnd, fadeInterval);
+        }
+    }
+}
