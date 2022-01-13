dcd8b007114ab6c0d43fb95da4d296081959726e
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-01-13 12:59:27 +0800

    Sequence Tester Scene
    To be Deleted in the future

---
+++ New Unity Project/Assets/Scripts/Logic/SequenceTester.cs
@@ -0,0 +1,242 @@
+﻿using System.Collections;
+using JondiBranchLogic;
+using ScriptableObjectScripts;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Logic
+{
+    public class SequenceTester : MonoBehaviour, IBattleSceneManager
+    {
+        #region Variables
+
+        
+
+       
+        
+        
+        /// <summary>
+        /// Reference to battle scene settings asset
+        /// </summary>
+        [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(IBattleSceneSettingsAsset))]
+        private Object battleSceneSettings;
+        public IBattleSceneSettingsAsset BattleSceneSettings
+        {
+            get => battleSceneSettings as IBattleSceneSettingsAsset;
+            set => battleSceneSettings = value as Object;
+        }
+        
+        /// <summary>
+        /// Returns this as a game object
+        /// </summary>
+        public GameObject ThisGameObject => this.gameObject;
+        
+        /// <summary>
+        /// Reference to Main Player
+        /// used in Initializing heroes
+        /// </summary>
+        public IPlayer MainPlayer { get; set; }
+        
+        /// <summary>
+        /// Reference to Enemy Player
+        ///  used in Initializing heroes
+        /// </summary>
+        public IPlayer EnemyPlayer { get; set; }
+        
+        /// <summary>
+        /// Reference to the the turn controller
+        /// set during initialization
+        /// </summary>
+        public ITurnController TurnController { get; set; }
+
+        /// <summary>
+        /// Reference to the game board
+        /// </summary>
+        public IGameBoard GameBoard { get; set; }
+        
+        /// <summary>
+        /// Reference to the end turn button
+        /// </summary>
+        public IEndTurnButton EndTurnButton { get; set; }
+
+        /// <summary>
+        /// Local variable for BranchLogic
+        /// used in initialization of global coroutine trees
+        /// </summary>
+        private IBranchLogic _branchLogic;
+        
+        /// <summary>
+        /// Local access to initialize turn controller script
+        /// </summary>
+        private IInitializeTurnController _initializeTurnController;
+        
+        /// <summary>
+        /// Local access to InitializePlayers attached
+        /// to the same gameObject
+        /// </summary>
+        private IInitializePlayers _initializePlayers;
+        
+        /// <summary>
+        /// Local access to initialize heroes
+        /// attached to the same game object
+        /// </summary>
+        private IInitializeHeroes _initializeHeroes;
+
+        /// <summary>
+        /// Local access to initialize game board script
+        /// </summary>
+        private IInitializeGameBoard _initializeGameBoard;
+    
+        /// <summary>
+        /// Local access to start battle
+        /// </summary>
+        private IStartBattle _startBattle;
+        
+        #endregion
+        
+        private void Awake()
+        {
+            _branchLogic = GetComponent<IBranchLogic>();
+        }
+
+        private void Start()
+        {
+            var logicTree = BattleSceneSettings.CoroutineTreesAsset.MainLogicTree;
+            
+            //logicTree.AddSibling(MainTestSequence());
+            MainTestSequence();
+        }
+
+        private void MainTestSequence()
+        {
+            var logicTree = BattleSceneSettings.CoroutineTreesAsset.MainLogicTree;
+            
+            logicTree.AddSibling(CoroutineA());
+            
+            logicTree.AddSibling(CoroutineB());
+            
+            logicTree.AddSibling(CoroutineC());
+            
+            
+            logicTree.EndSequence();
+            //yield return null;
+
+        }
+        
+        private IEnumerator CoroutineA()
+        {
+            var logicTree = BattleSceneSettings.CoroutineTreesAsset.MainLogicTree;
+            
+            Debug.Log("Coroutine A");
+            
+            logicTree.AddSibling(CoroutineA1());
+            
+            logicTree.AddSibling(CoroutineA2());
+            
+            logicTree.EndSequence();
+            yield return null;
+
+        }
+        
+        private IEnumerator CoroutineA1()
+        {
+            var logicTree = BattleSceneSettings.CoroutineTreesAsset.MainLogicTree;
+            
+            Debug.Log("Coroutine A.1");
+            
+            logicTree.AddSibling(CoroutineA11());
+            
+            logicTree.AddSibling(CoroutineA12());
+            
+            logicTree.EndSequence();
+            yield return null;
+
+        }
+        
+        private IEnumerator CoroutineA11()
+        {
+            var logicTree = BattleSceneSettings.CoroutineTreesAsset.MainLogicTree;
+            
+            Debug.Log("Coroutine A.1.1");
+            
+            logicTree.EndSequence();
+            yield return null;
+
+        }
+        
+        private IEnumerator CoroutineA12()
+        {
+            var logicTree = BattleSceneSettings.CoroutineTreesAsset.MainLogicTree;
+            
+            Debug.Log("Coroutine A.1.2");
+            
+            logicTree.EndSequence();
+            yield return null;
+
+        }
+        
+        private IEnumerator CoroutineA2()
+        {
+            var logicTree = BattleSceneSettings.CoroutineTreesAsset.MainLogicTree;
+            
+            Debug.Log("Coroutine A.2");
+            
+            logicTree.AddSibling(CoroutineA21());
+            
+            logicTree.AddSibling(CoroutineA22());
+            
+            logicTree.EndSequence();
+            yield return null;
+
+        }
+        
+        private IEnumerator CoroutineA21()
+        {
+            var logicTree = BattleSceneSettings.CoroutineTreesAsset.MainLogicTree;
+            
+            Debug.Log("Coroutine A.2.1");
+            
+            logicTree.EndSequence();
+            yield return null;
+
+        }
+        
+        private IEnumerator CoroutineA22()
+        {
+            var logicTree = BattleSceneSettings.CoroutineTreesAsset.MainLogicTree;
+            
+            Debug.Log("Coroutine A.2.2");
+            
+            logicTree.EndSequence();
+            yield return null;
+
+        }
+
+
+        private IEnumerator CoroutineB()
+        {
+            var logicTree = BattleSceneSettings.CoroutineTreesAsset.MainLogicTree;
+            
+            Debug.Log("Coroutine B");
+            
+            logicTree.EndSequence();
+            yield return null;
+
+        }
+        
+        private IEnumerator CoroutineC()
+        {
+            var logicTree = BattleSceneSettings.CoroutineTreesAsset.MainLogicTree;
+            
+            Debug.Log("Coroutine C");
+            
+            logicTree.EndSequence();
+            yield return null;
+
+        }
+        
+
+
+
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Logic/SequenceTester2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine;
+
+namespace Logic
+{
+    public class SequenceTester2 : MonoBehaviour
+    {
+        private void Start()
+        {
+            //GeneralMethods();
+        }
+
+        private void GeneralMethods()
+        {
+            MethodA();
+            MethodB();
+            MethodC();
+        }
+        
+        private void MethodA()
+        {
+            Debug.Log("Method A");
+            
+            MethodA1();
+            MethodA2();
+
+        }
+        
+        private void MethodA1()
+        {
+            Debug.Log("Method A.1");
+            MethodA11();
+            MethodA12();
+        }
+        
+        private void MethodA2()
+        {
+            Debug.Log("Method A.2");
+            MethodA21();
+            MethodA22();
+        }
+
+        private void MethodA11()
+        {
+            Debug.Log("Method A.1.1");
+        }
+        
+        private void MethodA12()
+        {
+            Debug.Log("Method A.1.2");
+        }
+        
+        private void MethodA21()
+        {
+            Debug.Log("Method A.2.1");
+        }
+        
+        private void MethodA22()
+        {
+            Debug.Log("Method A.2.2");
+        }
+        
+        private void MethodB()
+        {
+            Debug.Log("Method B");
+        }
+        
+        private void MethodC()
+        {
+            Debug.Log("Method C");
+        }
+        
+        
+    }
+}
