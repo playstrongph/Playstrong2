7b8d5b9d38154a4656941959ec313dd92305e4ee
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-01-20 15:43:57 +0800

    status effect lists

---
+++ New Unity Project/Assets/Scripts/Logic/IStatusEffectLists.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Logic
+{
+    public interface IStatusEffectLists
+    {
+        /// <summary>
+        /// Returns the list of status effect objects as a list of IStatusEffect
+        /// </summary>
+        List<IStatusEffect> StatusEffects { get; }
+
+        /// <summary>
+        /// Add status effect to interface and object list
+        /// </summary>
+        /// <param name="statusEffect"></param>
+        void AddToList(IStatusEffect statusEffect);
+
+        /// <summary>
+        /// Remove status effect to interface and object list
+        /// </summary>
+        /// <param name="statusEffect"></param>
+        void RemoveFromList(IStatusEffect statusEffect);
+    }
+}
