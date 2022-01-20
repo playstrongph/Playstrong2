6f4cd7f3f92ff4adc95e283fabbf4ef22f6d9e5c
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-01-20 17:03:17 +0800

    Status Effect Type Asset WIP

---
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/StatusEffectTypeAssets/IStatusEffectTypeAsset.cs
@@ -0,0 +1,21 @@
+﻿using Logic;
+
+namespace ScriptableObjectScripts.StatusEffectTypeAssets
+{
+    public interface IStatusEffectTypeAsset
+    {
+        /// <summary>
+        /// Add to the specific status effect list - i.e. buff, debuff, unique status effects
+        /// </summary>
+        /// <param name="heroStatusEffects"></param>
+        /// <param name="heroStatusEffect"></param>
+        void AddToStatusEffectsList(IHeroStatusEffects heroStatusEffects, IStatusEffect heroStatusEffect);
+
+        /// <summary>
+        /// Remove from the specific status effect list - i.e. buff, debuff, unique status effects
+        /// </summary>
+        /// <param name="heroStatusEffects"></param>
+        /// <param name="heroStatusEffect"></param>
+        void RemoveFromStatusEffectsList(IHeroStatusEffects heroStatusEffects, IStatusEffect heroStatusEffect);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/StatusEffectTypeAssets/StatusEffectTypeAsset.cs
@@ -0,0 +1,30 @@
+﻿using Logic;
+using UnityEngine;
+
+namespace ScriptableObjectScripts.StatusEffectTypeAssets
+{
+    public abstract class StatusEffectTypeAsset : ScriptableObject, IStatusEffectTypeAsset
+    {
+        /// <summary>
+        /// Add to the specific status effect list - i.e. buff, debuff, unique status effects
+        /// </summary>
+        /// <param name="heroStatusEffects"></param>
+        /// <param name="heroStatusEffect"></param>
+        public virtual void AddToStatusEffectsList(IHeroStatusEffects heroStatusEffects, IStatusEffect heroStatusEffect)
+        {
+            
+        }
+        
+        /// <summary>
+        /// Remove from the specific status effect list - i.e. buff, debuff, unique status effects
+        /// </summary>
+        /// <param name="heroStatusEffects"></param>
+        /// <param name="heroStatusEffect"></param>
+        public virtual void RemoveFromStatusEffectsList(IHeroStatusEffects heroStatusEffects, IStatusEffect heroStatusEffect)
+        {
+            
+        }
+        
+        
+    }
+}
