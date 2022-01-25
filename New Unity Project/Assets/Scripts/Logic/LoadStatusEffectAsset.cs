c38889e0fbf77e80740420f72a72bfb96d76f464
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-01-25 16:55:26 +0800

    Update LoadStatusEffectAsset.cs

--- New Unity Project/Assets/Scripts/Logic/LoadStatusEffectAsset.cs
+++ New Unity Project/Assets/Scripts/Logic/LoadStatusEffectAsset.cs
@@ -16,7 +16,27 @@ namespace Logic
 
         private void StartAction(IHero targetHero, IHero casterHero, IStatusEffectAsset statusEffectAsset, int counters)
         {
-           
+            _statusEffect.StatusEffectName = statusEffectAsset.StatusEffectName;
+            _statusEffect.StatusEffectDescription = statusEffectAsset.Description;
+            _statusEffect.CountersValue = statusEffectAsset.CountersValue;
+            _statusEffect.Icon.sprite = statusEffectAsset.Icon;
+            
+            _statusEffect.StatusEffectCasterHero = casterHero;
+            _statusEffect.StatusEffectTargetHero = targetHero;
+
+            //STATUS EFFECT ATTRIBUTES
+            
+            //Types - buff, debuff, unique status effect
+            _statusEffect.StatusEffectType = statusEffectAsset.StatusEffectType;
+            
+            //Counters timing update - start or end turn
+            _statusEffect.StatusEffectCounterUpdateType = statusEffectAsset.StatusEffectCounterUpdateType;
+            
+            //Instance types - singe or multiple
+            _statusEffect.StatusEffectInstanceType = statusEffectAsset.StatusEffectInstanceType;
+            
+            //Change counters type - normal, immutable, no change
+            _statusEffect.StatusEffectCounterType = statusEffectAsset.StatusEffectCounterType;
         }
     }
 }
