c4a7176ec7deea09e86a602e7d664ac873eea989
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-02-03 14:38:50 +0800

    Update hero preview

--- New Unity Project/Assets/Scripts/Logic/UpdateHeroPreview.cs
+++ New Unity Project/Assets/Scripts/Logic/UpdateHeroPreview.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Logic
 {
@@ -18,16 +20,46 @@ namespace Logic
         {
             var heroAttributes = _heroPreview.Hero.HeroLogic.HeroAttributes;
             
-            var baseAttackText = heroAttributes.BaseAttack.ToString();
-            var baseHealthText = heroAttributes.BaseHealth.ToString();
-            var baseSpeedText = heroAttributes.BaseSpeed.ToString();
-            var baseChanceText = heroAttributes.BaseChance.ToString();
+            var baseAttack = heroAttributes.BaseAttack;
+            var baseHealth = heroAttributes.BaseHealth;
+            var baseSpeed = heroAttributes.BaseSpeed;
+            var baseChance = heroAttributes.BaseChance;
+            var attack = heroAttributes.Attack;
+            var health = heroAttributes.Health;
+            var speed = heroAttributes.Speed;
+            var chance = heroAttributes.Chance;
 
-            //BASIC ATTRIBUTES
-            _heroPreview.HeroPreviewAttack.PreviewText.text = baseAttackText;
-            _heroPreview.HeroPreviewHealth.PreviewText.text = baseHealthText;
-            _heroPreview.HeroPreviewSpeed.PreviewText.text = baseSpeedText;
-            _heroPreview.HeroPreviewChance.PreviewText.text = baseChanceText;
+
+            //BASE BASIC ATTRIBUTES
+            
+            //update preview attack text and color
+            _heroPreview.HeroPreviewAttack.PreviewText.text = attack.ToString();
+            SetTextColor(baseAttack,attack,_heroPreview.HeroPreviewAttack.PreviewText);
+            
+            //update preview health text and color
+            _heroPreview.HeroPreviewHealth.PreviewText.text = health.ToString();
+            SetTextColor(baseHealth, health, _heroPreview.HeroPreviewHealth.PreviewText);
+            
+            //update preview speed text and color
+            _heroPreview.HeroPreviewSpeed.PreviewText.text = speed.ToString();
+            SetTextColor(baseSpeed,speed, _heroPreview.HeroPreviewSpeed.PreviewText);
+            
+            //update preview chance text and color
+            _heroPreview.HeroPreviewChance.PreviewText.text = chance.ToString();
+            SetTextColor(baseChance,chance, _heroPreview.HeroPreviewChance.PreviewText);
+            
+        }
+        
+        private void SetTextColor(int baseValue, int value, TextMeshProUGUI text)
+        {
+            if(value>baseValue)
+                text.color = Color.green;
+            else if (value == baseValue)
+                text.color = Color.white;
+            else if(value < baseValue)
+                text.color = Color.red;
+            else
+                text.color = Color.white;
         }
     }
 }
