be679e3d0cd1a2ae6b9f9ecf803c20492d41bf99
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-02-04 13:51:57 +0800

    Invinciblity WIP

---
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/BasicActionAssets/IncreaseTakeAllDamageReductionAsset.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using Logic;
+using UnityEngine;
+
+namespace ScriptableObjectScripts.BasicActionAssets
+{
+    
+    
+    /// <summary>
+    /// Increases the damage reduction factor for all types of take damage
+    /// Example - Invincibility
+    /// </summary>
+    [CreateAssetMenu(fileName = "IncreaseTakeAllDamageReduction", menuName = "Assets/BasicActions/I/IncreaseTakeAllDamageReduction")]
+    public class IncreaseTakeAllDamageReductionAsset : BasicActionAsset
+    {
+        /// <summary>
+        /// Value of damage reduction
+        /// </summary>
+        [SerializeField] private int value = 0;
+        
+        public override IEnumerator ExecuteAction(IHero hero)
+        {
+            var logicTree = hero.CoroutineTrees.MainLogicTree;
+            var damageAttributes = hero.HeroLogic.DamageAttributes;
+
+            damageAttributes.AllTakeDamageReduction += value;
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        public override IEnumerator UndoExecuteAction(IHero hero)
+        {
+            var logicTree = hero.CoroutineTrees.MainLogicTree;
+            var damageAttributes = hero.HeroLogic.DamageAttributes;
+
+            damageAttributes.AllTakeDamageReduction -= value;
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+
+    }
+}
