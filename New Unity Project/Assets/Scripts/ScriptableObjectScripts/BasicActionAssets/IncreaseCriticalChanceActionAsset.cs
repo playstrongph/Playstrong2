3370c442e9b1f4d8392cfa6461ca37fb308c07bd
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-03-19 11:24:25 +0800

    Critical Strike WIP

---
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/BasicActionAssets/IncreaseCriticalChanceActionAsset.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using DG.Tweening;
+using Logic;
+using ScriptableObjectScripts.GameAnimationAssets;
+using ScriptableObjectScripts.StandardActionAssets;
+using TMPro;
+using UnityEngine;
+
+namespace ScriptableObjectScripts.BasicActionAssets
+{
+    [CreateAssetMenu(fileName = "IncreaseCriticalChanceAction", menuName = "Assets/BasicActions/I/IncreaseCriticalChanceAction")]
+    public class IncreaseCriticalChanceActionAsset : BasicActionAsset
+    {   
+       
+        /// <summary>
+        /// Increase value by a percentage attack amount
+        /// </summary>
+        [SerializeField] private int criticalChance = 100;
+
+        /// <summary>
+        /// The specific logic-visual sequence for basic action
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <param name="targetHero"></param>
+        /// <returns></returns>
+        protected override IEnumerator MainBasicActionPhase(IHero casterHero, IHero targetHero)
+        {
+            var logicTree = casterHero.CoroutineTrees.MainLogicTree;
+
+            //base class method that calls execute action after checking life status and inability status
+            logicTree.AddCurrent(MainAction(casterHero));
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+
+        /// <summary>
+        /// Increase critical chance logic execution
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <param name="targetHero"></param>
+        /// <returns></returns>
+        public override IEnumerator ExecuteAction(IHero casterHero,IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTrees.MainLogicTree;
+
+            targetHero.HeroLogic.ChanceAttributes.CriticalChance += criticalChance;
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <param name="targetHero"></param>
+        /// <returns></returns>
+        public override IEnumerator UndoExecuteAction(IHero casterHero, IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTrees.MainLogicTree;
+           
+            targetHero.HeroLogic.ChanceAttributes.CriticalChance -= criticalChance;
+            
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+    }
+}
