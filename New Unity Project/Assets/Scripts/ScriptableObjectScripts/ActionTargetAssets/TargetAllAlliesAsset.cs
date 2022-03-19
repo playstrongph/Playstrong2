70ab1558f07b644ac0d63e23b6a6ab1ef7e10af5
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-03-19 14:16:41 +0800

    Evasion

---
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/ActionTargetAssets/TargetAllAlliesAsset.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Logic;
+using UnityEngine;
+
+namespace ScriptableObjectScripts.ActionTargetAssets
+{
+    [CreateAssetMenu(fileName = "TargetAllAllies", menuName = "Assets/ActionTargets/TargetAllAllies")]
+    public class TargetAllAlliesAsset : ActionTargetAsset
+    {
+        /// <summary>
+        /// Returns the hero's targeted hero
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <param name="targetHero"></param>
+        /// <returns></returns>
+        public override List<IHero> GetActionTargets(IHero casterHero,IHero targetHero)
+        {
+            //Should be from the perspective of the caster hero
+            var allAllies = casterHero.Player.AliveHeroes.Heroes;
+
+            var actionTargets = ShuffleList(new List<IHero>(allAllies));
+            
+            return actionTargets;
+        }
+        
+        public override List<IHero> GetEventSubscribers(IHero casterHero)
+        {
+            //Should be from the perspective of the caster hero
+            var allAllies = casterHero.Player.AliveHeroes.Heroes;
+
+            var actionTargets = ShuffleList(new List<IHero>(allAllies));
+            
+            return actionTargets;
+            
+        }
+        
+        
+    }
+}
