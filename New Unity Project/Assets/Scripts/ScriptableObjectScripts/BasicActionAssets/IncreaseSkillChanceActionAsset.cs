a1309f04cd9c37f004b660ab94df83750d3733db
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-03-23 13:50:04 +0800

    Chance

---
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/BasicActionAssets/IncreaseSkillChanceActionAsset.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using DG.Tweening;
+using Logic;
+using ScriptableObjectScripts.GameAnimationAssets;
+using ScriptableObjectScripts.StandardActionAssets;
+using TMPro;
+using UnityEngine;
+
+namespace ScriptableObjectScripts.BasicActionAssets
+{
+    [CreateAssetMenu(fileName = "IncreaseSkillChanceAction", menuName = "Assets/BasicActions/I/IncreaseSkillChanceAction")]
+    public class IncreaseSkillChanceActionAsset : BasicActionAsset
+    {   
+       
+        /// <summary>
+        /// Increase value by a percentage amount
+        /// </summary>
+        [SerializeField] private int bonusChance = 100;
+
+        /// <summary>
+        /// The specific logic-visual sequence for basic action
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <param name="targetHero"></param>
+        /// <returns></returns>
+        protected override IEnumerator MainBasicActionPhase(IHero casterHero, IHero targetHero)
+        {
+            var logicTree = casterHero.CoroutineTrees.MainLogicTree;
+
+            //base class method that calls execute action after checking life status and inability status
+            logicTree.AddCurrent(MainAction(casterHero));
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+
+        /// <summary>
+        /// Increase chance logic execution
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <param name="targetHero"></param>
+        /// <returns></returns>
+        public override IEnumerator ExecuteAction(IHero casterHero,IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTrees.MainLogicTree;
+
+            targetHero.HeroLogic.HeroAttributes.Chance += bonusChance;
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        
+        public override IEnumerator UndoExecuteAction(IHero casterHero, IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTrees.MainLogicTree;
+           
+            targetHero.HeroLogic.HeroAttributes.Chance -= bonusChance;
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+    }
+}
