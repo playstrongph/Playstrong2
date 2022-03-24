0188bcb06ccdc816fed0ef4f0cf0e6d902480d47
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-03-24 10:12:00 +0800

    Increase Target Attack Resistance

---
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/BasicActionAssets/IncreaseTargetAttackResistanceActionAsset.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using DG.Tweening;
+using Logic;
+using ScriptableObjectScripts.GameAnimationAssets;
+using ScriptableObjectScripts.StandardActionAssets;
+using TMPro;
+using UnityEngine;
+
+namespace ScriptableObjectScripts.BasicActionAssets
+{
+    [CreateAssetMenu(fileName = "IncreaseTargetAttackResistanceAction", menuName = "Assets/BasicActions/I/IncreaseTargetAttackResistanceAction")]
+    public class IncreaseTargetAttackResistanceActionAsset : BasicActionAsset
+    {   
+       
+        /// <summary>
+        /// Increase attack resistance
+        /// </summary>
+        [SerializeField] private int attackResistance = 0;
+
+        /// <summary>
+        /// The specific logic-visual sequence for basic action
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <param name="targetHero"></param>
+        /// <returns></returns>
+        protected override IEnumerator MainBasicActionPhase(IHero casterHero, IHero targetHero)
+        {
+            var logicTree = casterHero.CoroutineTrees.MainLogicTree;
+
+            //base class method that calls execute action after checking life status and inability status
+            logicTree.AddCurrent(MainAction(casterHero));
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+
+        /// <summary>
+        /// Increase the attribute value
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <param name="targetHero"></param>
+        /// <returns></returns>
+        public override IEnumerator ExecuteAction(IHero casterHero,IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTrees.MainLogicTree;
+
+            targetHero.HeroLogic.ResistanceAttributes.TargetAttackResistance += attackResistance;
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        
+        /// <summary>
+        /// Return the attribute value back to its last value
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <param name="targetHero"></param>
+        /// <returns></returns>
+        public override IEnumerator UndoExecuteAction(IHero casterHero, IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTrees.MainLogicTree;
+           
+            targetHero.HeroLogic.ResistanceAttributes.TargetAttackResistance -= attackResistance;
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+    }
+}
--- New Unity Project/Assets/Scripts/ScriptableObjectScripts/SkillTargetsAssets/SkillTargetEnemiesAsset.cs
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/SkillTargetsAssets/SkillTargetEnemiesAsset.cs
@@ -12,15 +12,78 @@ namespace ScriptableObjectScripts.SkillTargetsAssets
             var allEnemies = new List<IHero>(hero.Player.OtherPlayer.AliveHeroes.Heroes);
             var validTargets = new List<IHero>();
 
-            foreach (var enemyHero in allEnemies)
+            /*foreach (var enemyHero in allEnemies)
             {
                 var attackTargetResistance = enemyHero.HeroLogic.ResistanceAttributes.TargetAttackResistance;
                 if (attackTargetResistance <= 0 )
                     validTargets.Add(enemyHero);
-            }
+            }*/
+
+            validTargets = GetTargets(hero);
             
             return validTargets;
         }
+        
+        /// <summary>
+        /// Sorts out stealth and taunt heroes
+        /// </summary>
+        /// <returns></returns>
+        private List<IHero> GetTargets(IHero hero)
+        {
+            var finalTargets = new List<IHero>();
+            var tauntEnemies = new List<IHero>();
+            var stealthEnemies = new List<IHero>();
+            var allEnemies = new List<IHero>(hero.Player.OtherPlayer.AliveHeroes.Heroes);
+
+            //Initially, the final targets are all enemies
+            finalTargets = allEnemies;
+            
+            //Remove stealth targets from the final targets
+            foreach (var enemy in allEnemies)
+            {
+                if (enemy.HeroLogic.ResistanceAttributes.TargetAttackResistance > 0)
+                {
+                    stealthEnemies.Add(enemy);
+                    finalTargets.Remove(enemy);
+                }
+            }
+            
+            //If there are no targets left, assign stealth enemies as final targets
+            if (finalTargets.Count <= 0)
+                finalTargets = stealthEnemies;
+            
+            //Get all taunt targets
+            foreach (var enemy in allEnemies)
+            {
+                if(enemy.HeroLogic.ResistanceAttributes.TargetAttackResistance < 0)
+                    tauntEnemies.Add(enemy);
+            }
+            
+            //Assign only taunt enemies as the final targets 
+            if (tauntEnemies.Count > 0)
+            {
+                finalTargets.Clear();
+                finalTargets = tauntEnemies;
+            }
+
+            return finalTargets;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         /// <summary>
         /// Displays the enemy glow - red
         /// </summary>
