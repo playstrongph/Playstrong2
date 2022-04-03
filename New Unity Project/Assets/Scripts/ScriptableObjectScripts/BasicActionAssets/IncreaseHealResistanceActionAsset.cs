34d4f91a2dee27e4554a40e7e3e4c6e15fdf2be7
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-04-03 13:24:33 +0800

    unhealable

---
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/BasicActionAssets/IncreaseHealResistanceActionAsset.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using DG.Tweening;
+using Logic;
+using ScriptableObjectScripts.GameAnimationAssets;
+using ScriptableObjectScripts.StandardActionAssets;
+using TMPro;
+using UnityEngine;
+
+namespace ScriptableObjectScripts.BasicActionAssets
+{
+    [CreateAssetMenu(fileName = "IncreaseHealResistanceAction", menuName = "Assets/BasicActions/I/IncreaseHealResistanceAction")]
+    public class IncreaseHealResistanceActionAsset : BasicActionAsset
+    {   
+        /// <summary>
+        /// Increase value by a fixed amount
+        /// </summary>
+        [SerializeField] private int flatValue = 0;
+
+        /// <summary>
+        /// The specific logic-visual sequence for basic action
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <param name="targetHero"></param>
+        /// <returns></returns>
+        protected override IEnumerator MainBasicActionPhase(IHero casterHero, IHero targetHero)
+        {
+            var logicTree = casterHero.CoroutineTrees.MainLogicTree;
+
+            //base class method that calls execute action after checking life status and inability status
+            logicTree.AddCurrent(MainAction(casterHero));
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+
+        /// <summary>
+        /// Increase attack logic execution
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <param name="targetHero"></param>
+        /// <returns></returns>
+        public override IEnumerator ExecuteAction(IHero casterHero,IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTrees.MainLogicTree;
+
+            targetHero.HeroLogic.ResistanceAttributes.HealResistance += flatValue;
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        public override IEnumerator UndoExecuteAction(IHero casterHero, IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTrees.MainLogicTree;
+            
+            targetHero.HeroLogic.ResistanceAttributes.HealResistance -= flatValue;
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+    }
+}
