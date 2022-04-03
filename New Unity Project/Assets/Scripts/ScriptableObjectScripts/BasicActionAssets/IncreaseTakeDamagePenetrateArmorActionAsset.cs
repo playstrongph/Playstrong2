97842a39611ec9907bf77f6e2fe205486e0f911f
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-04-03 14:08:44 +0800

    Defense Break

---
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/BasicActionAssets/IncreaseTakeDamagePenetrateArmorActionAsset.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using DG.Tweening;
+using Logic;
+using ScriptableObjectScripts.GameAnimationAssets;
+using ScriptableObjectScripts.StandardActionAssets;
+using TMPro;
+using UnityEngine;
+
+namespace ScriptableObjectScripts.BasicActionAssets
+{
+    [CreateAssetMenu(fileName = "IncreaseTakeDamagePenetrateArmorAction", menuName = "Assets/BasicActions/I/IncreaseTakeDamagePenetrateArmorAction")]
+    public class IncreaseTakeDamagePenetrateArmorActionAsset : BasicActionAsset
+    {   
+       
+        /// <summary>
+        /// Increase value by a percentage attack amount
+        /// </summary>
+        [SerializeField] private int flatValue = 0;
+
+        /// <summary>
+        /// The specific logic-visual sequence for basic action
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <param name="targetHero"></param>
+        /// <returns></returns>
+        protected override IEnumerator MainBasicActionPhase(IHero casterHero, IHero targetHero)
+        {
+            var logicTree = casterHero.CoroutineTrees.MainLogicTree;
+
+            //base class method that calls execute action after checking life status and inability status
+            logicTree.AddCurrent(MainAction(casterHero));
+
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+
+        /// <summary>
+        /// Increase value 
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <param name="targetHero"></param>
+        /// <returns></returns>
+        public override IEnumerator ExecuteAction(IHero casterHero,IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTrees.MainLogicTree;
+
+            targetHero.HeroLogic.DamageAttributes.TakeDamagePenetrateArmor += flatValue;
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+        
+        
+        /// <summary>
+        /// Return to normal
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <param name="targetHero"></param>
+        /// <returns></returns>
+        public override IEnumerator UndoExecuteAction(IHero casterHero, IHero targetHero)
+        {
+            var logicTree = targetHero.CoroutineTrees.MainLogicTree;
+           
+            targetHero.HeroLogic.DamageAttributes.TakeDamagePenetrateArmor -= flatValue;
+            
+            logicTree.EndSequence();
+            yield return null;
+        }
+
+    }
+}
