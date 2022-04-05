6687ff4d51023c2011ecffdf17fdee5c1c3a8f19
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-04-05 13:01:44 +0800

    Seal WIP
    Absolute All allies, all enemies

---
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/ActionTargetAssets/TargetAllAlliesAsset.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Logic;
+using UnityEngine;
+
+namespace ScriptableObjectScripts.ActionTargetAssets
+{
+    [CreateAssetMenu(fileName = "TargetAllAllies", menuName = "Assets/ActionTargets/TargetAllAllies")]
+    public class TargetAllAlliesAsset : ActionTargetAsset
+    {
+        /// <summary>
+        /// Returns the hero's targeted hero
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <param name="targetHero"></param>
+        /// <returns></returns>
+        public override List<IHero> GetActionTargets(IHero casterHero,IHero targetHero)
+        {
+            //Should be from the perspective of the caster hero
+            var allLivingAllies = casterHero.Player.AliveHeroes.Heroes;
+            var allDeadAllies = casterHero.Player.DeadHeroes.Heroes;
+            var allExtinctAllies = casterHero.Player.ExtinctHeroes.Heroes;
+            
+            var allAllies = new List<IHero>();
+            allAllies.AddRange(allLivingAllies);
+            allAllies.AddRange(allDeadAllies);
+            allAllies.AddRange(allExtinctAllies);
+
+            var actionTargets = ShuffleList(new List<IHero>(allAllies));
+            
+            return actionTargets;
+        }
+        
+        public override List<IHero> GetEventSubscribers(IHero casterHero)
+        {
+            //Should be from the perspective of the caster hero
+            var allLivingAllies = casterHero.Player.AliveHeroes.Heroes;
+            var allDeadAllies = casterHero.Player.DeadHeroes.Heroes;
+            var allExtinctAllies = casterHero.Player.ExtinctHeroes.Heroes;
+            
+            var allAllies = new List<IHero>();
+            allAllies.AddRange(allLivingAllies);
+            allAllies.AddRange(allDeadAllies);
+            allAllies.AddRange(allExtinctAllies);
+
+            var actionTargets = ShuffleList(new List<IHero>(allAllies));
+            
+            return actionTargets;
+            
+        }
+        
+        
+    }
+}
---
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/ActionTargetAssets/TargetAllEnemiesAsset.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Logic;
+using UnityEngine;
+
+namespace ScriptableObjectScripts.ActionTargetAssets
+{
+    [CreateAssetMenu(fileName = "TargetAllEnemies", menuName = "Assets/ActionTargets/TargetAllEnemies")]
+    public class TargetAllEnemiesAsset : ActionTargetAsset
+    {
+        /// <summary>
+        /// Returns the hero's targeted hero
+        /// </summary>
+        /// <param name="casterHero"></param>
+        /// <param name="targetHero"></param>
+        /// <returns></returns>
+        public override List<IHero> GetActionTargets(IHero casterHero,IHero targetHero)
+        {
+            //Should be from the perspective of the caster hero
+            var allLivingEnemies = casterHero.Player.OtherPlayer.AliveHeroes.Heroes;
+            var allDeadEnemies = casterHero.Player.OtherPlayer.DeadHeroes.Heroes;
+            var allExtinctEnemies = casterHero.Player.OtherPlayer.ExtinctHeroes.Heroes;
+            
+            var allEnemies = new List<IHero>();
+            allEnemies.AddRange(allLivingEnemies);
+            allEnemies.AddRange(allDeadEnemies);
+            allEnemies.AddRange(allExtinctEnemies);
+
+            var actionTargets = ShuffleList(new List<IHero>(allEnemies));
+            
+            return actionTargets;
+        }
+        
+        public override List<IHero> GetEventSubscribers(IHero casterHero)
+        {
+            //Should be from the perspective of the caster hero
+            var allLivingEnemies = casterHero.Player.OtherPlayer.AliveHeroes.Heroes;
+            var allDeadEnemies = casterHero.Player.OtherPlayer.DeadHeroes.Heroes;
+            var allExtinctEnemies = casterHero.Player.OtherPlayer.ExtinctHeroes.Heroes;
+            
+            var allEnemies = new List<IHero>();
+            allEnemies.AddRange(allLivingEnemies);
+            allEnemies.AddRange(allDeadEnemies);
+            allEnemies.AddRange(allExtinctEnemies);
+
+            var actionTargets = ShuffleList(new List<IHero>(allEnemies));
+            
+            return actionTargets;
+            
+        }
+        
+        
+    }
+}
