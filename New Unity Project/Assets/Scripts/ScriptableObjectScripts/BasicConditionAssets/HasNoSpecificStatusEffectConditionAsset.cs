2a05f42793b96a591f617794c0b4cd9dbbac7536
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-05-28 13:25:59 +0800

    EXTERMINATION

---
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/BasicConditionAssets/HasNoSpecificStatusEffectConditionAsset.cs
@@ -0,0 +1,43 @@
+﻿using Logic;
+using ScriptableObjectScripts.StatusEffectAssets;
+using UnityEngine;
+
+namespace ScriptableObjectScripts.BasicConditionAssets
+{
+    [CreateAssetMenu(fileName = "HasNoSpecificStatusEffectCondition", menuName = "Assets/BasicConditions/H/HasNoSpecificStatusEffectCondition")]
+    public class HasNoSpecificStatusEffectConditionAsset : BasicConditionAsset
+    {
+        [Header("Does Not Have This Specific Status Effect")]
+        [SerializeField] private ScriptableObject statusEffectAsset;
+        
+        private IStatusEffectAsset StatusEffectAsset
+        {
+            get => statusEffectAsset as IStatusEffectAsset;
+            set => statusEffectAsset = value as ScriptableObject;
+        }
+
+        protected override int CheckConditionValue(IHero hero)
+        {
+            //Default is False
+            var value = 0;
+
+            var statusEffects = hero.HeroStatusEffects.GetAllStatusEffects(hero);
+            
+            //Check if status effect is present
+            foreach (var statusEffect in statusEffects)
+            {
+                if (statusEffect.StatusEffectName == StatusEffectAsset.StatusEffectName)
+                    value = 1;
+            }
+            
+            
+            
+            //Returns 0 if status effect is present and 1 if not present
+            value = value == 1 ? 0 : 1;
+            
+            Debug.Log("Has No Specific Status Effect. Value: " +value);
+
+            return value;
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/BasicConditionAssets/HasSpecificStatusEffectConditionAsset.cs
@@ -0,0 +1,38 @@
+﻿using Logic;
+using ScriptableObjectScripts.StatusEffectAssets;
+using UnityEngine;
+
+namespace ScriptableObjectScripts.BasicConditionAssets
+{
+    [CreateAssetMenu(fileName = "HasSpecificStatusEffectCondition", menuName = "Assets/BasicConditions/H/HasSpecificStatusEffectCondition")]
+    public class HasSpecificStatusEffectConditionAsset : BasicConditionAsset
+    {
+        [Header("Has This Specific Status Effect")]
+        [SerializeField] private ScriptableObject statusEffectAsset;
+        
+        private IStatusEffectAsset StatusEffectAsset
+        {
+            get => statusEffectAsset as IStatusEffectAsset;
+            set => statusEffectAsset = value as ScriptableObject;
+        }
+
+        protected override int CheckConditionValue(IHero hero)
+        {
+            //Default is False
+            var value = 0;
+            
+            var statusEffects = hero.HeroStatusEffects.GetAllStatusEffects(hero);
+            
+            //If status effect is present
+            foreach (var statusEffect in statusEffects)
+            {
+                if (statusEffect.StatusEffectName == StatusEffectAsset.StatusEffectName)
+                    value = 1;
+            }
+            
+            Debug.Log("Has Specific Status Effect. Value: " +value);
+            
+            return value;
+        }
+    }
+}
