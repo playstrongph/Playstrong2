4d7583c130e25bd53053d3bb94e0646636ba8e9e
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-05-31 06:16:37 +0800

    Immunity Attributes WIP

--- New Unity Project/Assets/Scripts/Logic/IImmunityAttributes.cs
+++ New Unity Project/Assets/Scripts/Logic/IImmunityAttributes.cs
@@ -1,7 +1,25 @@
-﻿namespace Logic
+﻿using System.Collections.Generic;
+using ScriptableObjectScripts.StatusEffectAssets;
+using UnityEngine;
+
+namespace Logic
 {
     public interface IImmunityAttributes
     {
+
+        List<ScriptableObject> DebuffScriptableObjects { get; }
+
+        List<IStatusEffectAsset> Debuffs { get; }
+        
+        List<ScriptableObject> BuffScriptableObjects { get; }
+
+        List<IStatusEffectAsset> Buffs { get; }
+        
+        List<ScriptableObject> UniqueStatusEffectsScriptableObjects { get; }
+
+        List<IStatusEffectAsset> UniqueStatusEffects { get; }
+        
+
         int StunEffectsImmunity { get; set; }
     }
 }
--- New Unity Project/Assets/Scripts/Logic/ImmunityAttributes.cs
+++ New Unity Project/Assets/Scripts/Logic/ImmunityAttributes.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using ScriptableObjectScripts.StatusEffectAssets;
 using UnityEngine;
 
 namespace Logic
@@ -14,11 +16,84 @@ namespace Logic
         {
             _heroLogic = GetComponent<IHeroLogic>();
         }
+
+
+        [Header("STATUS EFFECT IMMUNITIES")] 
         
+        [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(IStatusEffectAsset))]
+        private List<ScriptableObject> debuffs = new List<ScriptableObject>();
         
-        [Header("Debuff Immunities")]
-      
+        /// <summary>
+        /// "debuffs" scriptable object setter
+        /// </summary>
+        public List<ScriptableObject> DebuffScriptableObjects => debuffs;
         
+        public List<IStatusEffectAsset> Debuffs
+        {
+            get
+            {
+                var newDebuffs = new List<IStatusEffectAsset>();
+
+                foreach (var debuffObject in debuffs)
+                {
+                    var debuff = debuffObject as IStatusEffectAsset;
+                    newDebuffs.Add(debuff);
+                }
+
+                return newDebuffs;
+            }
+        }
+        
+        [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(IStatusEffectAsset))]
+        private List<ScriptableObject> buffs = new List<ScriptableObject>();
+        
+        /// <summary>
+        /// "Buffs" scriptable object setter
+        /// </summary>
+        public List<ScriptableObject> BuffScriptableObjects => buffs;
+
+        public List<IStatusEffectAsset> Buffs
+        {
+            get
+            {
+                var newBuffs = new List<IStatusEffectAsset>();
+
+                foreach (var buffObject in buffs)
+                {
+                    var buff = buffObject as IStatusEffectAsset;
+                    newBuffs.Add(buff);
+                }
+
+                return newBuffs;
+            }
+        }
+        
+        [SerializeField] [RequireInterfaceAttribute.RequireInterface(typeof(IStatusEffectAsset))]
+        private List<ScriptableObject> uniqueStatusEffects = new List<ScriptableObject>();
+        
+        /// <summary>
+        /// "uniqueStatusEffects" scriptable object setter
+        /// </summary>
+        public List<ScriptableObject> UniqueStatusEffectsScriptableObjects => uniqueStatusEffects;
+
+        public List<IStatusEffectAsset> UniqueStatusEffects
+        {
+            get
+            {
+                var newUniqueStatusEffects = new List<IStatusEffectAsset>();
+
+                foreach (var uniqueStatusEffectObject in uniqueStatusEffects)
+                {
+                    var uniqueStatusEffect = uniqueStatusEffectObject as IStatusEffectAsset;
+                    newUniqueStatusEffects.Add(uniqueStatusEffect);
+                }
+                
+                return newUniqueStatusEffects;
+            }
+        }
+
+
+
         [SerializeField ]private int stunEffectsImmunity;
         public int StunEffectsImmunity
         {
