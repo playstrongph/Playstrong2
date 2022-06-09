146adcaa5b9d12eeae80d6e4f3ee28f9d0bb2d5d
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-06-09 15:08:10 +0800

    FULL BURST WIP

---
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/BasicConditionAssets/FightingSpiritValuesConditionAsset.cs
@@ -0,0 +1,50 @@
+﻿using AssetsScriptableObjects;
+using Logic;
+using ScriptableObjectScripts.StatusEffectAssets;
+using UnityEngine;
+
+namespace ScriptableObjectScripts.BasicConditionAssets
+{
+    [CreateAssetMenu(fileName = "FightingSpiritValuesCondition", menuName = "Assets/BasicConditions/F/FightingSpiritValuesCondition")]
+    public class FightingSpiritValuesConditionAsset : BasicConditionAsset
+    {
+        [Header("Negative value means limit is DISABLED")]
+        [Header("Set Limit Value")]
+        
+        //Negative limit means it's disabled
+        [SerializeField] private int lessThanLimit = -9999999;
+        
+        //Big limit value means it's disabled
+        [SerializeField] private int greaterThanLimit = 9999999;
+        
+        //Negative limit means it's disabled
+        [SerializeField] private int equalToLimit = -99999999;
+
+        protected override int CheckConditionValue(IHero hero)
+        {
+            //Initialize to false
+            var value = 0;
+
+            var fightingSpirit = hero.HeroLogic.HeroAttributes.FightingSpirit;
+            
+            Debug.Log("Hero: " +hero.HeroName);
+
+            if (fightingSpirit < lessThanLimit)
+            {
+                value = 1;
+            }
+
+            if (fightingSpirit > greaterThanLimit)
+            {
+                value = 1;  //Condition is met
+            }
+
+            if (fightingSpirit == equalToLimit)
+            {
+                value = 1;  //Condition is met
+            }
+
+            return value;
+        }
+    }
+}
