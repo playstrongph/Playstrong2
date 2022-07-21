fad480171358416cd8a089ab38d3d62984d4834e
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-07-21 11:00:57 +0800

    Skill Target All

---
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/SkillTargetsAssets/SkillTargetAllAsset.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Logic;
+using UnityEngine;
+
+namespace ScriptableObjectScripts.SkillTargetsAssets
+{
+    [CreateAssetMenu(fileName = "SkillTargetAll", menuName = "Assets/SkillTargets/SkillTargetAll")]
+    public class SkillTargetAllAsset : SkillTargetsAsset
+    {
+        public override List<IHero> HeroTargets(IHero hero)
+        {
+            var allHeroes = new List<IHero>(hero.Player.AliveHeroes.Heroes);
+            
+            allHeroes.AddRange(hero.Player.OtherPlayer.AliveHeroes.Heroes);
+            
+            return allHeroes;
+        }
+        
+        /// <summary>
+        /// Displays the ally glow - yellow
+        /// </summary>
+        /// <param name="hero"></param>
+        public override void ShowHeroGlow(IHero hero)
+        {
+            hero.HeroVisual.SetHeroFrameAndGlow.CurrentHeroFrame.EnableAllyLightAndGlow(); 
+        }
+        
+        /// <summary>
+        /// Hides the ally glow - yellow
+        /// </summary>
+        /// <param name="hero"></param>
+        public override void HideHeroGlow(IHero hero)
+        {
+            hero.HeroVisual.SetHeroFrameAndGlow.CurrentHeroFrame.DisableAllyLightAndGlow();
+        }
+        
+        
+    }
+}
