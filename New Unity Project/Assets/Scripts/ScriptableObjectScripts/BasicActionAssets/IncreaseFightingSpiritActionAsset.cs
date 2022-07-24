d967adf1bf04ff8b83ee8820c6e3d35fc1cf5e37
playstrongph <44939646+playstrongph@users.noreply.github.com>
2022-07-24 13:16:15 +0800

    Landy Update

--- New Unity Project/Assets/Scripts/ScriptableObjectScripts/BasicActionAssets/IncreaseFightingSpiritActionAsset.cs
+++ New Unity Project/Assets/Scripts/ScriptableObjectScripts/BasicActionAssets/IncreaseFightingSpiritActionAsset.cs
@@ -98,8 +98,7 @@ namespace ScriptableObjectScripts.BasicActionAssets
                 var fightingSpirit = targetHero.HeroLogic.HeroAttributes.FightingSpirit + finalFightingSpiritDeltaValue;
 
                 targetHero.HeroLogic.SetFightingSpirit.StartAction(fightingSpirit);
-                
-                
+
             }
 
             logicTree.EndSequence();
